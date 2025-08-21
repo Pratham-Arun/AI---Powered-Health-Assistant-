1bae34a3db8a59c95db478ec7d4c9398130c4132
Pratham Arun <prathamarun7@gmail.com>
2025-08-22 01:23:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import pytz,os,sys
 import datetime
 
 
-
 from dotenv import load_dotenv
 
 # Load environment variables from a .env file
@@ -527,4 +526,3 @@ if st.session_state.auth_token:
                 st.session_state.clear_history_confirm = False
 else:
     st.markdown("<h2 style='text-align: center;'>🔒 Please log in to access the chatbot.</h2>", unsafe_allow_html=True)
-
